c978e67319ba2cc40b68a6da5070d4ab2a71556a
SIVASANGARI1219 <sangari3619@gmail.com>
2026-03-28 10:56:07 +0530

    Time: 0 ms (100%), Space: 19.4 MB (15.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,7 +1,20 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+
+        # counting the occurances like 2 is occuring 2 times...
         c0, c1, c2 = nums.count(0), nums.count(1), nums.count(2)
-        nums[:c0] = [0]*c0
+        
+        # comparing and swaping elemets
+        #    values*n 
+        nums[:c0] = [0]*c0                
         nums[c0:c0+c1] = [1]*c1
         nums[c0+c1:] = [2]*c2
+
+
+
+
+
+c0 = 2   # two 0s
+c1 = 2   # two 1s
+c2 = 2   # two 2s
   
